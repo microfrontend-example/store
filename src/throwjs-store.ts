c235ef5898728f60962f4d0fd27cd13c98936f8d
Carlos Morales <dev.carlos.morales@gmail.com>
2022-09-24 17:38:35 -0500

    feat: update  import

--- src/throwjs-store.ts
+++ src/throwjs-store.ts
@@ -9,10 +9,12 @@ export interface ITodo {
 class StoreTodo {
   private _storeTodo$: BehaviorSubject<ITodo[]>;
   private _id: number;
+  private _key: string;
 
   constructor() {
-    this._storeTodo$ = new BehaviorSubject([]);
     this._id = 0;
+    this._key = "TODOS";
+    this._storeTodo$ = new BehaviorSubject(this.getFromLocalStorage());
   }
 
   get storeTodo$(): Observable<ITodo[]> {
@@ -26,6 +28,7 @@ class StoreTodo {
   addTodo(todo: ITodo): void {
     this._storeTodo$.next([...this._storeTodo$.value, todo]);
     this._id++;
+    this.saveInLocalStorage();
   }
 
   changeCompleted(id: number): void {
@@ -34,18 +37,34 @@ class StoreTodo {
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
     this._storeTodo$.next([...todos]);
+    this.saveInLocalStorage();
   }
 
   deleteTodo(id: number): void {
     const todos = this.todos().filter((todo) => todo.id !== id);
     this._storeTodo$.next([...todos]);
+    this.saveInLocalStorage();
   }
 
   private todos(): ITodo[] {
     return this._storeTodo$.getValue();
   }
+
+  private saveInLocalStorage(): void {
+    localStorage.setItem(this._key, JSON.stringify(this.todos()));
+  }
+
+  private getFromLocalStorage(): ITodo[] {
+    const todos = localStorage.getItem(this._key);
+    if (todos) {
+      const todosParsed = JSON.parse(todos) as ITodo[];
+      this._id =
+        todosParsed.length > 0 ? todosParsed[todosParsed.length - 1].id + 1 : 0;
+      return todosParsed;
+    } else {
+      return [];
+    }
+  }
 }
 
-const storeTodo = new StoreTodo();
-
-export default storeTodo;
+export const storeTodo = new StoreTodo();
